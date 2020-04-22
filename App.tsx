b3bdd8838f9b5a3467735760d71732ab3efa0faa
Shawn Sullivan <sullivansm3@gmail.com>
2020-04-21 18:31:45 -0600

    Create bottom Nav

--- App.tsx
+++ App.tsx
@@ -1,45 +1,79 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Navbar from './app/ui/Navbar';
-import Signin from './app/ui/Signin';
+import Login from './app/ui/Login';
 
 function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <View style={styles.home}>
-        <Text style={styles.text}>Welcome to trackIt </Text>
-        <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
-          <Text>Sign in</Text>
-        </TouchableOpacity>
-      </View>
-      <Navbar />
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="Day" component={DayScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
   )
 }
 
-function SigninScreen() {
+{/* <View style={styles.container}>
+<View style={styles.home}>
+  <Text style={styles.text}>Welcome to trackIt </Text>
+</View>
+<Navbar />
+</View> */}
+
+function DayScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Day Screen...oooAAAAHHHHHH!</Text>
+      <Text>FIGHTER OF THE NIGHT SCREEN... oooAAAHHH!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+function LoginScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <View style={styles.home}>
-        <Text style={styles.text}>Please Sign in </Text>
-        <Signin />
+        <Text style={styles.text}>Please login</Text>
+        <Login />
+        <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+          <Text>Login</Text>
+        </TouchableOpacity>
       </View>
-      <Navbar />
     </View>
   )
 }
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator 
+        initialRouteName="Login"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#2A2D34',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      >
+        <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} options={{title: 'trackIt'}}/>
-        <Stack.Screen name="Signin" component={SigninScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -48,34 +82,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FBFBFF',
     alignItems: 'center',
     justifyContent: 'center',
     // marginTop: 40,
   },
   home: {
     flex: 10,
-    backgroundColor: 'lightgrey',
+    backgroundColor: '#FBFBFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
   text: {
-    color: 'blue',
+    color: '#01BAEF',
     fontSize: 30,
   }
 });
-
-
-// export default function App() {
-//   return (
-//     <NavigationContainer>
-//       <View style={styles.container}>
-//         <View style={styles.home}>
-//           <Text style={styles.text}>Welcome to trackIt </Text>
-//           <Signin />
-//         </View>
-//         <Navbar />
-//       </View>
-//     </NavigationContainer>
-//   );
-// }
