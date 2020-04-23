40c873808aa203da90a2a544bcf36be4c33eb19d
Shawn Sullivan <sullivansm3@gmail.com>
2020-04-23 14:45:54 -0600

    Create input fields for each tracking element

--- app/ui/DailyLog/DailyLog.js
+++ app/ui/DailyLog/DailyLog.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Button } from 'react-native';
 
 
 export class DailyLog extends React.Component {
@@ -12,10 +12,62 @@ export class DailyLog extends React.Component {
   //Post / Put route to enter data into the database
   //Allow user to view previous days to make updates
 
+  postLog(){
+
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>Daily Log</Text>
+        <Text style={styles.text}>Notes from the day</Text>
+        <View style={styles.notesContainer}>
+          <TextInput 
+            placeholder='Notes' 
+            style={styles.input}
+            // onChangeText={}
+          />
+        </View>
+        <Text style={styles.text}>Hours of Sleep</Text>
+        <View style={styles.inputContainer}>
+          <TextInput 
+            placeholder='Hours of sleep' 
+            style={styles.input}
+            // onChangeText={}
+          />
+        </View>
+        <Text style={styles.text}>Total Creative Work Hours</Text>
+        <View style={styles.inputContainer}>
+          <TextInput 
+            placeholder='Creative Work Hours' 
+            style={styles.input}
+            // onChangeText={}
+          />
+        </View>
+        <Text style={styles.text}>Subjective Feel today</Text>
+        <View style={styles.inputContainer}>
+          <TextInput 
+            placeholder='Subjective feel today' 
+            style={styles.input}
+            // onChangeText={}
+          />
+        </View>
+        <Text style={styles.text}>Read</Text>
+        <View style={styles.inputContainer}>
+          <TextInput 
+            placeholder='true/false' 
+            style={styles.input}
+            // onChangeText={}
+          />
+        </View>
+        <Text style={styles.text}>Workout</Text>
+        <View style={styles.inputContainer}>
+          <TextInput 
+            placeholder='true/false' 
+            style={styles.input}
+            // onChangeText={}
+          />
+        </View>
       </View>
     )
   }
@@ -31,6 +83,20 @@ const styles = StyleSheet.create({
     text: {
       color: '#01BAEF',
       fontSize: 30,
+    },
+    input: {
+      backgroundColor: '#ffffff',
+      paddingLeft: 15,
+      paddingRight: 15
+    },
+    inputContainer: {
+      borderWidth: 1,
+      width: 200, 
+    },
+    notesContainer: {
+      borderWidth: 1,
+      width: 200,
+      height: 100, 
     }
 });
 
