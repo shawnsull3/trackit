d6ed1af7e3cec9d69e529a2c35ff6bb690f1b34f
Shawn Sullivan <sullivansm3@gmail.com>
2020-04-25 10:58:23 -0600

    Round percentages to nearest whole number

--- app/ui/Metrics/Percentages.js
+++ app/ui/Metrics/Percentages.js
@@ -5,7 +5,7 @@ const Percentages = ({ logData, target }) => {
     const data = []
     logData.map(log => data.push(log[target]));
     const trueArr = data.filter(day => day === true);
-    const percent = (trueArr.length / data.length) * 100
+    const percent = Math.ceil((trueArr.length / data.length) * 100);
 
     return (
       <Text styles={{color: '#01BAEF'}}>{percent}%</Text>
