dd0ed4e4788f565d9d7485ed635c74feba1dbdf9
Shawn Sullivan <sullivansm3@gmail.com>
2020-04-24 19:47:02 -0600

    Render line charts of data

--- app/ui/Metrics/LineCharts.js
+++ app/ui/Metrics/LineCharts.js
@@ -1,46 +1,32 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
-import * as d3 from 'd3-scale';
-import { Svg, Rect, G, Line, Path, Defs, LinearGradient, Stop } from "react-native-svg";
-import c3 from 'c3';
+import { LineChart, Path, Grid } from 'react-native-svg-charts';
 
-const GRAPH_MARGIN = 20
-const colors = {
-  axis: 'black'
+const LineCharts = () => {
+    const data = [50, 10, 40, 95, -4, -24, 85, 91];
+    
+    const Shadow = ({ line }) => (
+      <Path
+        key={'shadow'}
+        y={2}
+        d={line}
+        fill={'none'}
+        strokeWidth={4}
+        stroke={'rgba(134, 65, 244, 0.2)'}
+      />
+    )
+    
+    return (
+      <LineChart
+        style={{ height: 200 }}
+        data={data}
+        svg={{ stroke: 'rgb(134, 65, 244)' }}
+        contentInset={{ top: 20, bottom: 20 }}
+      >
+        <Grid />
+        <Shadow/>
+      </LineChart>
+    )   
 }
 
-const LineCharts = ({ data }) => {
-  const SVGHeight = 300
-  const SVGWidth = 300
-  const graphHeight = SVGHeight - 2 * GRAPH_MARGIN
-  const graphWidth = SVGWidth - 2 * GRAPH_MARGIN
-
-  const xDomain = data.map(log => log.date)
-    const xRange = [0, graphWidth]
-    const x = d3.scalePoint()
-      .domain(xDomain)
-      .range(xRange)
-      .padding(1)
-
-
-  return (
-    <View>
-      <Svg width={SVGWidth} height={SVGHeight}>
-        {/* translate for 'graphHeight' on y axis */}
-        <G y={graphHeight}>
-            {/* bottom axis */}
-          <Line
-            x1="0"
-            y1="2"
-            x2={graphWidth}
-            y2="2"
-            stroke={colors.axis}
-            strokeWidth="0.5"
-          />
-        </G>
-      </Svg>
-    </View>
-  )
-}
-
-export default LineCharts
+export default LineCharts;
